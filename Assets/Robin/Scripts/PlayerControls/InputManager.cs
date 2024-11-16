0acef5c590e4bfcb71056d76d2638e389cc03c49
Nibour16 <ruobinh3@hotmail.com>
2024-11-15 23:54:53 -0700

    Player Movement

---
+++ Assets/Robin/Scripts/PlayerControls/InputManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private static InputManager _instance;
+
+    public static InputManager Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    private PlayerInput playerInput;
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+        playerInput = new PlayerInput();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void OnEnable()
+    {
+        playerInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInput.Disable();
+    }
+
+    public Vector2 GetPlayerMovement()
+    {
+        return playerInput.PlayerAction.Movement.ReadValue<Vector2>();
+    }
+}
