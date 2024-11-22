7e958adc420999d3c8147d6466b0916469aa91af
flamypow <wytchen90s@gmail.com>
2024-11-22 11:39:06 -0700

    Implement Basic State Machine, Added Follow logic.

---
+++ Assets/William/Scripts/FriendFollow.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FriendFollow : BaseState
+{
+    private FriendStateMachine _stateMachine;
+    public FriendFollow(FriendStateMachine stateMachine)
+    {
+        _stateMachine = stateMachine;
+    }
+    public override void EnterState()
+    {
+        
+    }
+
+    public override void ExitState()
+    {
+        
+    }
+
+    public override void UpdateState()
+    {
+        
+    }
+
+}
