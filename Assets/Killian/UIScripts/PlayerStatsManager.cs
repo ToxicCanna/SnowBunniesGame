1040739ce5e6053d10eae93c8c5d5faca04886de
ToxicCanna <freedom22140@gmail.com>
2024-11-28 12:59:45 -0700

    Hunger and Health Bars working in gym

--- Assets/Killian/UIScripts/PlayerStatsManager.cs
+++ Assets/Killian/UIScripts/PlayerStatsManager.cs
@@ -9,48 +9,44 @@ public class PlayerStatsManager : Singleton<PlayerStatsManager>
     private float warmthValue;
     private float maxWarmthValue = 100f;
 
+    private float hungerDrainRate = 1f; // Amount hunger decreases per second
+    private float warmthDrainRate = 0.5f; // Amount warmth decreases per second
+
+    public float GetHungerValue() => hungerValue;
+    public float GetMaxHungerValue() => maxHungerValue;
+    public float GetWarmthValue() => warmthValue;
+    public float GetMaxWarmthValue() => maxWarmthValue;
+
     public void Start()
     {
         hungerValue = 100f;
         warmthValue = 100f;
     }
 
-    public float GetHungerValue()
-    {
-        return hungerValue;
-    }
-    public float GetMaxHungerValue()
-    {
-        return maxHungerValue;
-    }
-
-    public float GetWarmthValue()
-    {
-        return warmthValue;
-    }
-    public float GetMaxWarmthValue()
-    {
-        return maxWarmthValue;
-    }
-
     public void AddValueToHunger(float value)
     {
-        hungerValue += value;
+        hungerValue = Mathf.Min(hungerValue + value, maxHungerValue);
     }
 
     public void AddValueToWarmth(float value)
     {
-        warmthValue += value;
+        warmthValue = Mathf.Min(warmthValue + value, maxWarmthValue);
     }
 
     public void SetWarmthToMax()
     {
-        warmthValue = 100f;
+        warmthValue = maxWarmthValue;
     }
 
     // Update is called once per frame
     void Update()
     {
-                
+        // Drain hunger and warmth over time
+        hungerValue -= hungerDrainRate * Time.deltaTime;
+        warmthValue -= warmthDrainRate * Time.deltaTime;
+
+        // Ensure values don't go below 0
+        hungerValue = Mathf.Max(0f, hungerValue);
+        warmthValue = Mathf.Max(0f, warmthValue);
     }
 }
--- Assets/Killian/UIScripts/WarmthBar.cs
+++ Assets/Killian/UIScripts/WarmthBar.cs
@@ -1,38 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HungerBar : MonoBehaviour
+public class WarmthBar : MonoBehaviour
 {
     [SerializeField] private Image _image;
     [SerializeField] private Gradient _gradient;
+
     private float _target;
-    
 
     // Start is called before the first frame update
-    private Coroutine drainHungerBarCoroutine;
     void Start()
     {
         _image = GetComponent<Image>();
-
-        CheckHungerBarGradientAmount();
+        CheckWarmthBarGradientAmount();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Update the target based on the player's hunger value
-        _target = PlayerStatsManager.Instance.GetHungerValue() / PlayerStatsManager.Instance.GetMaxHungerValue();
+        // Update the target based on the player's warmth value
+        _target = PlayerStatsManager.Instance.GetWarmthValue() / PlayerStatsManager.Instance.GetMaxWarmthValue();
 
-        // Update the fill amount of the hunger bar
+        // Update the fill amount of the warmth bar
         _image.fillAmount = Mathf.Lerp(_image.fillAmount, _target, Time.deltaTime * 2f); // Lerp the value smoothly over time
 
         // Update the color based on the fill amount
-        CheckHungerBarGradientAmount();
+        CheckWarmthBarGradientAmount();
     }
 
-    private void CheckHungerBarGradientAmount()
+    private void CheckWarmthBarGradientAmount()
     {
         _image.color = _gradient.Evaluate(_image.fillAmount);
     }
