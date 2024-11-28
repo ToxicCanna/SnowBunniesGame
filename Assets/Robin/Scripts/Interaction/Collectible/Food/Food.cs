adcf1924bb0b4455be51ddeea2245f05cbf4ef09
ToxicCanna <freedom22140@gmail.com>
2024-11-28 14:10:33 -0700

    staging patch 1

--- Assets/Robin/Scripts/Interaction/Collectible/Food/Food.cs
+++ Assets/Robin/Scripts/Interaction/Collectible/Food/Food.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -11,6 +11,7 @@ public class Food : UsingItem, IUsingItem
         {
             Destroy(gameObject);
             Debug.Log("I have eaten something");
+            PlayerStatsManager.Instance.AddValueToHunger(20);
         }
     }
 }
